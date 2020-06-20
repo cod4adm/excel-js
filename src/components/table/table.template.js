0ce91256026edccaf5defe12eb24d6d591923855
cod4adm <cod4adm@gmail.com>
2020-06-20 23:27:10 +0300

    Finish table resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,23 +3,30 @@ const CODES = {
     Z: 90
 };
 
-function toCell() {
+function toCell(_, col) {
     return `
-        <div class="cell" contenteditable></div>
+        <div class="cell" contenteditable data-col="${col}"></div>
         `
 }
 
-function toCol(col) {
+function toCol(col, index) {
     return `
-        <div class="column">${col}</div>
+        <div class="column" data-type="resizable" data-col="${index}">
+            ${col}
+            <div class="col-resize" data-resize="col"></div>
+        </div>
         `
 }
 
 function createRow(index, content) {
+    const resize = index ? '<div class="row-resize" data-resize="row"></div>' : '';
     return `
-        <div class="row">
-                <div class="row-info">${index ? index : ''}</div>
-                <div class="row-data">${content}</div>
+        <div class="row" data-type="resizable">
+          <div class="row-info">
+            ${index ? index : ''}
+            ${resize}
+          </div>
+          <div class="row-data">${content}</div>
         </div>
         `
 }
@@ -35,12 +42,12 @@ export function createTable(rowsCount = 15) {
     const cols = new Array(colsCount)
         .fill('')
         .map((el, index) => toChar(index))
-        .map(el => toCol(el))
-        . join('');
+        .map(toCol)
+        .join('');
 
     rows.push(createRow(null, cols));
 
-    for (let i=0; i<rowsCount; i++) {
+    for (let i = 0; i < rowsCount; i++) {
         const cells = new Array(colsCount)
             .fill('')
             .map(toCell)
